84dc666c629c19cd971e4cb06e74be384f778ec0
xireiki <xireiki@users.noreply.github.com>
2024-11-24 16:54:45 +0000

    v1.173.0-xireiki 19ff9d65ab2e1a22f5ac1b19f67477df49d81820

---
+++ assets/DocumentTitle-_Y4ITW6i.js
@@ -0,0 +1,1 @@
+import{d as t,bC as r}from"./index-BOmkt3j_.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
---
+++ assets/time-EM413FjU.js
@@ -0,0 +1,1 @@
+import{W as a,b4 as m}from"./index-BOmkt3j_.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
